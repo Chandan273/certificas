67cdf432291009c634b4eae113342abbbe8a6615
chandan <rajansnuriya273@gmail.com>
2023-02-03 00:29:56 +0530

    fix bugs

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,15 +3,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./routes";
 import 'devextreme/dist/css/dx.light.css';
-
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-const vuetify = createVuetify({
-  components,
-  directives,
-})
+import vuetify from '../plugins/vuetify'
 
 createApp(App).use(router).use(vuetify).mount('#app');
